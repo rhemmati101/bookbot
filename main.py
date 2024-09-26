eb6772d8004edd34f0c4dda272ce2c45f8b6f11e
rhemmati101 <rhemmati@g.ucla.edu>
2024-09-26 13:17:05 -0700

    streamlined sorting of chars in output

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ def main():
     
     contents = read_file(book_path)
     num_words = wordcount(contents)
-    char_counts_dict = character_count(contents)
+    char_counts = character_count(contents)
 
-    # convert char_counts dict to list of dicts with char and count keys for each char
-    char_counts_list = dict_to_list(char_counts_dict)
-
-    # sort char_counts to be printed in order of most common to least common char
-    char_counts_list.sort(reverse=True, key=lambda dict: dict["count"])
-
-    print_report(book_path, num_words, char_counts_list)
+    print(f"--- Report of {book_path} ---")
+    print(f"The document contains {num_words} words")
+    # goes through char keys in sorted order by frequency
+    for char in sorted(char_counts, reverse=True, key=lambda char: char_counts[char]):
+        print(f"The {char} character was found {char_counts[char]} times")
     
     return 0
 
@@ -22,36 +20,19 @@ def read_file(path_to_file):
 def wordcount(text):
     return len(text.split())
 
-# only tallies alphabetic chars
+# only tallies alphabetical characters
 def character_count(text):
-    counts = dict()
+    counts = {}
 
-    for char in text:
-        if not char.isalpha():
+    for character in text:
+        if not character.isalpha():
             continue
-        if char.lower() not in counts.keys():
-            counts[char.lower()] = 1
+        if character.lower() not in counts.keys():
+            counts[character.lower()] = 1
         else:
-            counts[char.lower()] += 1
+            counts[character.lower()] += 1
 
 
     return counts
-
-def print_report(book_path, num_words, char_counts_list):
-    print(f"--- Report of {book_path} ---")
-    print(f"The document contains {num_words} words")
-    for char_dict in char_counts_list:
-        print(f"The {char_dict['char']} character was found {char_dict['count']} times")
-
-def dict_to_list(char_counts_dict):
-    char_counts_list = []
-    for char in char_counts_dict:
-        ith_dict = {}
-        ith_dict["char"] = char
-        ith_dict["count"] = char_counts_dict[char]
-        
-        char_counts_list.append(ith_dict)
-
-    return char_counts_list
 
 main()
